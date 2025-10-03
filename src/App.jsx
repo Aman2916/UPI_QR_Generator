54e035990b31546a45e18344c01ba8021a81e1c1
Aman <malviyaaman412@gmail.com>
2025-10-03 22:42:12 +0530

    Added_form_to_Submit_IDs

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,41 @@
 import QRCode from "react-qr-code";
+import React from "react";
 
 function App() {
+  const [qrValue, setQrValue] = React.useState("");
+  const [name, setName] = React.useState("");
+
+  const changeQr = (e) => {
+    setName(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setQrValue(name);
+  };
+
   return (
     <div className="app">
       <h1>QR Generator</h1>
-      <QRCode
-        size={256}
-        style={{ height: "auto", maxWidth: "100%", width: "100%" }}
-        value="random"
-        viewBox={`0 0 256 256`}
-      />
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Enter your UPI ID"
+          style={{ marginBottom: 20, padding: 10, width: "80%" }}
+          value={name}
+          onChange={changeQr}
+        />
+        <button type="submit">Submit</button>
+      </form>
+
+      {qrValue && (
+        <QRCode
+          size={256}
+          style={{ height: "auto", maxWidth: "100%", width: "100%" }}
+          value={qrValue}
+          viewBox={`0 0 256 256`}
+        />
+      )}
     </div>
   );
 }
